1feb424b0af83db73fc7123d1dadbe39305d7d7d
Deepak <deepak481481481@gmail.com>
2024-08-27 13:31:10 +0530

    the project is ready to be hosted

--- styles & scripts/script.js
+++ styles & scripts/script.js
@@ -1,7 +1,9 @@
+// To show the sidebar 
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
+// To hide the sidebar 
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
